b3490a8aba6e2b8d856702afe4297e9f0c5bafd9
RIMRIM <96475505+li-moo@users.noreply.github.com>
2023-06-19 02:48:43 +0900

    Delete fe_ipb_app directory

--- fe_ipb_app/src/helpers/identifierValidator.js
+++
@@ -1,6 +0,0 @@
-export function identifierValidator(number) {
-  const re = /^\d+$/
-  if (!number) return "ID를 입력해주세요."
-  if (!re.test(number)) return '숫자로만 구성된 ID를 입력해주세요.'
-  return ''
-}
--- fe_ipb_app/src/helpers/passwordValidator.js
+++
@@ -1,5 +0,0 @@
-export function passwordValidator(password) {
-  if (!password) return "Password can't be empty."
-  if (password.length < 4) return '암호는 5자 이상이어야 합니다.'
-  return ''
-}
